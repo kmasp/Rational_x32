456c425f8cca86c6438d9db0b2f9e96d080e5b98
zuctut <ztukhtamishev@stuy.edu>
2015-11-18 11:28:04 -0500

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -1,11 +1,89 @@
 //Kyle Moon
 //APCS1 pd5
 //HW32
+public class Rational 
+{
+    // Instance Variables
+    private int _n;
+    private int _d;
 
-public class Rational {
-
-    public static void main (String[] args) {
-	System.out.println("Hello World");
+    // Constructors
+    public Rational()
+    {
+        _n = 0;
+        _d = 1;
     }
 
-} //Ending class Rational
+    public Rational(int n, int d)
+    {
+        this();
+        if (d != 0)
+        {
+            _n = n;
+            _d = d;
+        }
+        else
+            System.out.println("Invalid. Fraction set to 0 / 1");
+    }
+
+    // Accessor Methods
+    public int getN()
+    {
+        return _n;
+    }
+
+    public int getD()
+    {
+        return _d;
+    }
+
+    // Override toString() Method
+    public String toString()
+    {
+        return ( "Fraction: " + _n + " / " + _d + "\n");
+    }
+
+    // Float Value Method
+    public double floatValue()
+    {
+        return ( 1.0*_n / _d ); 
+    }
+
+    // Multiply Method
+    public void multiply(Rational fraction)
+    {
+        _n *= fraction.getN();
+        _d *= fraction.getD();
+    }
+
+    // Divide Method
+    public void divide(Rational fraction)
+    {
+        if ( fraction.getN() != 0 )
+        {
+            _n *= fraction.getD();
+            _d *= fraction.getN();
+        }
+        else
+            System.out.println("Divide by Zero error.");
+    }
+
+
+
+
+ public static void main(String[] args){
+      Rational r = new Rational(2,3); //Stores the rational number 2/3
+Rational s = new Rational(1,2); //Stores the rational number 1/2
+r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains ½
+System.out.println(r);
+r.divide(s);
+System.out.println(r);
+Rational z = new Rational(0,3);
+Rational k = new Rational(1,2);
+z.multiply(k);
+System.out.println(z);
+z.divide(k);
+System.out.println(z);
+
+    }
+}
