aa23664a6062bf2157b493b950a607c9f2b02def
Kyle Moon <kmoon@stuy.edu>
2015-11-18 01:35:21 -0500

    This is my first commit!

---
+++ Rational.java
@@ -0,0 +1,11 @@
+//Kyle Moon
+//APCS1 pd5
+//HW32
+
+public class Rational {
+
+    public static void main (String[] args) {
+	System.out.println("Hello World");
+    }
+
+} //Ending class Rational
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Moon, Kyle, 2718, kmasp, https://github.com/kmasp/Rational_x32
